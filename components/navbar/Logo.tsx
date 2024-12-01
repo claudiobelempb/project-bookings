003e23c5865c3da7be4a9672951129cb4ec58900
Claudio Cardoso <claudio.c.lima@hotmail.com>
2024-12-01 07:48:12 -0300

    NavSearch

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { LucideTent } from "lucide-react";
+import { LuTent } from "react-icons/lu";
 
 function Logo() {
   return (
     <Button size="icon" asChild>
       <Link href="/">
-        <LucideTent className="w-6 h-6" />
+        <LuTent className="w-6 h-6" />
       </Link>
     </Button>
   );
