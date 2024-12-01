b18ac8af7c1940a6030650fab518f87532401b50
Claudio Cardoso <claudio.c.lima@hotmail.com>
2024-12-01 07:39:12 -0300

    Logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { LucideTent } from "lucide-react";
 
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <LucideTent className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
